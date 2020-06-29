37245a73ead52efd033fd338d9d89cc3d40f4661
thesave9 <savemyemailid@gmail.com>
2020-06-29 23:51:36 +0530

    Add files via upload

---
+++ library/Thing.cs
@@ -0,0 +1,12 @@
+﻿// Thing.cs
+
+using static Newtonsoft.Json.JsonConvert;
+
+namespace Library
+{
+  public class Thing
+  {
+    public int Get(int left, int right) =>
+        DeserializeObject<int>($"{left + right}");
+  }
+}
